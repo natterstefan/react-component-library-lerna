08082b9ceebaa37d775fc696949cc13224af47cf
natterstefan <stefan@natter.at>
2019-02-10 19:40:57 +0100

    implement simple useState (react hook) example

--- packages/button/src/index.stories.js
+++ packages/button/src/index.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { withKnobs, text } from '@storybook/addon-knobs'
@@ -8,10 +8,26 @@ import ButtonReadme from '../README.md'
 
 import Button from '.'
 
-// the only reason onClick was written like this, and not just
-// `onClick = action('clicked')` is due to a cleaner export in the JSX tab
-const onClick = e => {
-  action('clicked')(e)
+const onClickHandler = e => action('clicked')(e)
+
+const StoryComponent = () => {
+  // Declare a new state variable, which we'll call "count" with inital state 0
+  const [count, setCount] = useState(0)
+
+  const buttonText = text('text', 'Click me')
+  // the only reason onClick was written like this, and not just
+  // `onClick = action('clicked')` is due to a cleaner export in the JSX tab
+  const onClick = e => {
+    setCount(count + 1)
+    onClickHandler(e)
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <Button onClick={onClick}>{buttonText}</Button>
+    </div>
+  )
 }
 
 storiesOf('Button', module)
@@ -19,5 +35,6 @@ storiesOf('Button', module)
   .addDecorator(withReadme(ButtonReadme))
   .addWithJSX('default', () => {
     const buttonText = text('text', 'Click me')
-    return <Button onClick={onClick}>{buttonText}</Button>
+    return <Button onClick={onClickHandler}>{buttonText}</Button>
   })
+  .addWithJSX('with click counter (hooks)', () => <StoryComponent />)
